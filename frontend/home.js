18645951365e8e17c8edae5f149937d58b121fa9
palkim <d7deniz@windowslive.com>
2020-08-08 18:09:03 +0300

    minimum viable

--- frontend/home.js
+++ frontend/home.js
@@ -10,6 +10,7 @@ class FramesLoader {
 */
 
 class FramePlayer {
+    //TODO refactor frameloading, progressbar and state functionalities out (to sub classes?)
     constructor(html_id, video_id = 1) {
         /*
         // if this type of private var is good idea
@@ -22,7 +23,7 @@ class FramePlayer {
         //TODO look up js class getter setter best practices
         this.html_id = html_id
         this.video_id = video_id
-        this.is_playing = false
+        this.video_state = VideoStateEnum.INIT
         this.img_element = document.getElementById(this.html_id);
         this._triggers = {}; // string eventname -> [func] callbacks
         this.frames = [] //init with undefined?
@@ -30,7 +31,7 @@ class FramePlayer {
         this.progress=0 // 0-100, current progress of the progress bar
         //this.last_loaded_frame_index = 0
 
-        document.getElementById('myBar').addEventListener('click', this.set_position) //todo change id
+        document.getElementById('myProgress').addEventListener('click', this.set_position) //todo change id
 
         this.getVideoUrls().then(urls => {
             let frame_count = urls.length * FRAME_PER_IMAGE;
@@ -38,7 +39,7 @@ class FramePlayer {
             this.frames=[]
 
             //init the interval
-            this.frame_index = 0
+            this.frame_index = 0;
             // TODO do this here and lazy load setInterval(this.interval_method.bind(this), 3000);
 
             //start getting frames
@@ -48,9 +49,13 @@ class FramePlayer {
                 this.getBase64Async(urls[i]).then(img_base64 => {
                     console.log("fetching image "+i.toString())
                     this.frames_from_image(img_base64);
+
                 })
             }
 
+            //TODO FrameLoader().then(setframe; setinterval; fireevent;)
+            //this.set_frame(this.frames[0])
+            this.triggerHandler(VideoEventsEnum.DOWNLADED);
             setInterval(this.interval_method.bind(this), 100);
 
         })
@@ -64,16 +69,23 @@ class FramePlayer {
 
     //accesing this., 3 ways, https://stackoverflow.com/questions/2001920/calling-a-class-prototype-method-by-a-setinterval-event
     interval_method() {
-        if(!this.is_playing){
+        if(this.video_state !== VideoStateEnum.PLAYING ){
             return;
         }
-        console.log("framing interval method: ")
-        console.log(this.frame_index)
+
+        //console.log("framing interval method: ")
+        //console.log(this.frame_index)
         //console.log(this.frames)
         //console.log(this.frames[this.frame_index])
         this.set_frame(this.frames[this.frame_index])
         this.frame_index = this.frame_index + 1
         this.move_progress()
+        if(this.frame_index === this.frames.length){
+            //TODO refactor moveout to this.end_video()
+            this.video_state = VideoStateEnum.END;
+            this.triggerHandler(VideoEventsEnum.END);
+            return;
+        }
     }
 
     frames_from_image(image_data_base64) { // returns [ base64 ]
@@ -81,26 +93,46 @@ class FramePlayer {
         // for better readability image is created here everytime
         // if performance matters just put it out there above
         let image = new Image()
-        image.onload = this.cutImageUp(image, this.frames) //returns a function with no arguments
+        image.onload = this.cutImageUp(image, this.frames, this.video_state, this.set_frame, this) //returns a function with no arguments //yeah bad shit, TODO
         image.src = image_data_base64
         //crop images into 25 smaller ones
     }
 
-
-    play() {
-        this.is_playing = true
+    changestate(new_state){
+        // TODO assert new_state is of type VideoStateEnum
     }
 
-    pause() {
-        this.is_playing = false
+    play() { //private
+        this.video_state = VideoStateEnum.PLAYING
+        this.triggerHandler(VideoEventsEnum.PLAY);
     }
 
-    playpause(){
-        this.is_playing = !this.is_playing;
+    pause() { //private
+        this.video_state = VideoStateEnum.PAUSE
+        this.triggerHandler(VideoEventsEnum.PAUSE);
+    }
+
+    show_icon(icon_id){
+
+    }
+
+    playpause(){ //public
+        if(this.video_state == VideoStateEnum.PLAYING) {
+            this.pause();
+        }
+        else if(this.video_state == VideoStateEnum.PAUSE || this.video_state == VideoStateEnum.STARTING){
+            this.play();
+        }
+        else if(this.video_state==VideoStateEnum.END){
+            this.progress = 0;
+            this.frame_index=0;
+            this.play()
+        }
+
     }
 
 
-    cutImageUp(image, frames) {// returns [ base64 ]
+    cutImageUp(image, frames, video_state, set_frame_fnc, thiss) {// returns [ base64 ]
         return function () {
             //console.log("image source: "+image.src)
             for (let y = 0; y < 5; ++y) {
@@ -126,6 +158,12 @@ class FramePlayer {
                     frames.push(toadd);
                 }
             }
+            //weird way to do this, TODO, do this in the constructor or something
+            if(video_state == VideoStateEnum.INIT){
+                thiss.video_state = VideoStateEnum.STARTING;
+                set_frame_fnc(frames[0]);
+            }
+            //
             let anImageElement = document.getElementById("frames");
         }
 
@@ -139,6 +177,7 @@ class FramePlayer {
     }
 
     triggerHandler(event_name) {
+        //TODO add ms as argument
         if (this._triggers[event_name]) {
             //fun fact, callbacks.forEach(Function.prototype.call, Function.prototype.call); https://zpao.com/posts/calling-an-array-of-functions-in-javascript/
             for (let i of this._triggers[event_name]) {
@@ -173,7 +212,7 @@ class FramePlayer {
             })
             //.then(response => Buffer.from(response.data, 'binary').toString('base64')) //for nodejs
             .then(response => this._imageEncode(response.data)) //should be Buffer.from for nodejs>6
-    }
+    } //TODO bad naming, fix convention
 
     set_frame(img_base64) {
         //("framing set frame: ", img_base64)
@@ -183,36 +222,58 @@ class FramePlayer {
             );
     }
 
+
+    set_bar_position(progress, width){
+        let elem = document.getElementById("myBar");
+        elem.style.width = width + "%";
+        elem.innerHTML = Math.round(progress)  + "%";
+    }
+
     move_progress() {
         if(this.progress>=100){
             return;
         }
-
-        let elem = document.getElementById("myBar");
         let width = 0;
         let raise_by = 100/this.frames.length
         this.progress+=raise_by;
         let progress = this.progress;
+        let thiss = this;
         let id = setInterval(frame, 10); //TODO make this with FPS
         function frame() {
-            if (width >= raise_by) {
+            if (width >= raise_by || thiss.video_state != VideoStateEnum.PLAYING) {
                 clearInterval(id);
             } else {
                 width+=raise_by/10; //TODO make this with FPS
-                elem.style.width = progress+width + "%";
-                elem.innerHTML = Math.round(progress)  + "%";
+                thiss.set_bar_position(progress, progress+width);
             }
         }
 
     }
 
     set_position = e => {
-        let rect = e.target.getBoundingClientRect();
+        //this.video_state = VideoStateEnum.PAUSE; //style choice, may be removed
+        this.pause() //do we want event fire here? //style choice, may be removed
+        let rect;
+        let totalwidth;
+        if(e.target.id == "myProgress" ){ //hack, clicked on myProgress (outer bar)
+            rect = e.target.children[0].getBoundingClientRect();
+            totalwidth = e.target.getBoundingClientRect().width
+        }
+        else{ //clicked on myBar (inner bar)
+            rect = e.target.getBoundingClientRect();
+            totalwidth = e.target.parentElement.getBoundingClientRect().width
+        }
 
-        let x = ((e.clientX - rect.left)/(e.target.parentElement.getBoundingClientRect().width) )*100;
+
+
+        let x = ((e.clientX - rect.left)/totalwidth )*100;
         //var y = e.clientY - rect.top;
-        this.progress = Math.max(Math.floor(x)-1,0); //cosmetic buffer
+        //this.progress = Math.max(x-0.5,0); //cosmetic buffer, disabled
+        this.progress = x;
         this.frame_index = Math.floor(this.frames.length*this.progress/100);
+        this.set_frame(this.frames[this.frame_index])
+        this.set_bar_position(this.progress, this.progress)
+
         let a = 32*43;
 
     }
@@ -225,12 +286,16 @@ let img_frame = undefined;
 document.addEventListener("DOMContentLoaded", function () {
 
     img_frame = new FramePlayer("frameImg", 1)
+    // TODO, make 3 have ms argument
+    img_frame.on(VideoEventsEnum.PLAY, () => console.log("playing, event") )
+    img_frame.on(VideoEventsEnum.PAUSE, () => console.log("pausing, event") )
+    img_frame.on(VideoEventsEnum.END, () => console.log("ended, event") )
+    img_frame.on(VideoEventsEnum.DOWNLADED, () => console.log("downloaded, event") )
 
 
 });
 
 function OnClickFrameArea(arg) {
-    console.log(arg)
     img_frame.playpause()
 }
 
--- frontend/utils.js
+++ frontend/utils.js
@@ -2,3 +2,19 @@ function range(n) {
     //alternatives (yes the link is weird and works) https://2ality.com/2018/12/creating-arrays.html#:~:text=One%20common%20way%20of%20creating,equal(arr.
     return Array.from({length: n}, (x, i) => i)
 }
+
+//carry this to inside FramePlayer class?
+const VideoStateEnum = {
+    INIT: 1,
+    STARTING: 2,
+    PLAYING: 3,
+    PAUSE: 4,
+    END: 5
+};
+
+const VideoEventsEnum = {
+    DOWNLADED: "ondownloadcomplete",
+    PLAY: "onplay",
+    PAUSE: "onpause",
+    END: "onend",
+};
